197e6e2a95bd1a74aa020bd0e6b2dc042c4bbc55
FluffyDoesStuff <cizak57@gmail.com>
2023-02-13 22:07:26 -0500

    Complete App Redesign

---
+++ app/src/main/java/com/team2137/chargedupscouting/2023Scoutingv2/app/src/main/java/com/example/a2023_scouting_v2/ui/home/HomeFragment.java
@@ -0,0 +1,37 @@
+package com.example.a2023_scouting_v2.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.a2023_scouting_v2.databinding.FragmentAutoBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentAutoBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentAutoBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textAutoTop;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
